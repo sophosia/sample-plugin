617bcb104836cc1c2c33cc7c7caa5ece509bf2af
HuntFeng <spacecurvature@gmail.com>
2024-03-08 23:32:18 -0600

    do not externalize vue

--- vite.config.ts
+++ vite.config.ts
@@ -15,22 +15,10 @@ export default defineConfig({
   build: {
     lib: {
       // src/indext.ts is where we have exported the component(s)
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "src/main.ts"),
       name: "SamplePlugin",
       // the name of the output files when the build is run
       fileName: "main"
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue"
-        }
-      }
     }
   }
 });
